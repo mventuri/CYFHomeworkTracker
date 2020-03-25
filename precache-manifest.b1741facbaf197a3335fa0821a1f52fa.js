761516ff90847b3f248230af2656f9a5902c6430
Chris Owen <chris.owen@codeyourfuture.io>
2020-03-25 23:05:30 +0000

    Updates

---
+++ precache-manifest.b1741facbaf197a3335fa0821a1f52fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04692d95793639f084786db989898a72",
+    "url": "/cyf-homework-tracker/index.html"
+  },
+  {
+    "revision": "6f1ef3e324ba80ade71e",
+    "url": "/cyf-homework-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b4e3df900f95a6dc6dd9",
+    "url": "/cyf-homework-tracker/static/js/2.92d70eb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cyf-homework-tracker/static/js/2.92d70eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1ef3e324ba80ade71e",
+    "url": "/cyf-homework-tracker/static/js/main.6ed34293.chunk.js"
+  },
+  {
+    "revision": "b140abf0c935e8e146aa",
+    "url": "/cyf-homework-tracker/static/js/runtime-main.7de84937.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cyf-homework-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cyf-homework-tracker/precache-manifest.b1741facbaf197a3335fa0821a1f52fa.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cyf-homework-tracker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
