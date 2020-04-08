d114fd627661fa917d9fdc98bf0e29fd8f6dc368
Chris Owen <cowen1992@hotmail.co.uk>
2020-04-08 22:51:35 +0100

    Updates

--- precache-manifest.626a6500814e9a0df0fc236a25683d2b.js
+++ precache-manifest.626a6500814e9a0df0fc236a25683d2b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "046793a97e9f90a72a660e311039448f",
+    "revision": "e8ef2ea4cd80ddd3f2dcbb672debf76b",
     "url": "/CYFHomeworkTracker/index.html"
   },
   {
-    "revision": "1de7aeef76a6f00a39d0",
+    "revision": "a96617ae80dbc87f0ca6",
     "url": "/CYFHomeworkTracker/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/CYFHomeworkTracker/static/js/2.edc4154e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1de7aeef76a6f00a39d0",
-    "url": "/CYFHomeworkTracker/static/js/main.ed142fdf.chunk.js"
+    "revision": "a96617ae80dbc87f0ca6",
+    "url": "/CYFHomeworkTracker/static/js/main.146e4735.chunk.js"
   },
   {
     "revision": "0c8f4710a52dead5e2ab",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CYFHomeworkTracker/precache-manifest.1fbb3402c0140c49be5f08e78e50dcbf.js"
+  "/CYFHomeworkTracker/precache-manifest.626a6500814e9a0df0fc236a25683d2b.js"
 );
 
 self.addEventListener('message', (event) => {
